5acb59f81a3a50e8757bbe86066327178ece5f62
th11n <thinex@wp.pl>
2023-08-23 19:47:46 +0200

    feat: api

--- api/index.py
+++ api/index.py
@@ -1,7 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import StreamingResponse
+import subprocess
+import os
+import uuid
+from fastapi.middleware.cors import CORSMiddleware
+import io
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "OPTIONS"],
+    allow_headers=["Content-Type"],
+)
+
+@app.post("/api/convert")
+async def convert_audio_format(
+    file: UploadFile = File(...),
+    format: str = Form(...)
+):
+    input_format, output_format = format.split("_")
+
+    try:
+        file_content = await file.read()
+
+        temp_second_file = f"temp_{str(uuid.uuid4())}.{output_format}"
+        subprocess.run(["ffmpeg", "-i", "-", temp_second_file], input=file_content, check=True)
+
+        response = StreamingResponse(io.BytesIO(file_content), media_type=f"audio/{output_format}")
+        response.headers["Content-Disposition"] = f"attachment; filename=converted.{output_format}"
+
+        return response
+
+    except Exception as e:
+        return {"error": str(e)}
